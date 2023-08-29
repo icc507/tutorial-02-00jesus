c219b117b11ddc3b1071b7e38dd9dbcd9f45058c
jesus <j.acevedo03@ufromail.cl>
2023-08-29 09:48:03 -0400

    Update del problema2

--- problema2.py
+++ problema2.py
@@ -7,10 +7,11 @@
 #         20 90 hola jiji 77
 #La salida debe ser
 #         (77, 'jiji', 'hola', 90, 20)
+# Lectura de elementos
 t = input().split()
-inversa = []
-for i in t:
-    inversa.insert(0, i)
 
-tupla = tuple(inversa)
+inverso = t[::-1]
+tupla = tuple(int(indice) if indice.isdigit() else indice for indice in inverso)
+
 print(tupla)
+
